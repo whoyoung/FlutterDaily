69744020242fed728f460ac242f0bec920ca245f
yanghu <yanghu@keep.com>
2019-06-25 10:39:22 +0800

    添加 listview

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,32 @@
 import 'package:flutter/material.dart';
 
 // import 'package:flutter_daily/demos/flutter_channel.dart';
-import 'package:flutter_daily/demos/increment.dart';
+// import 'package:flutter_daily/demos/increment.dart';
 
 void main() {
 
   // runApp(MaterialApp(home: PlatformChannel()));
 
-  runApp(MaterialApp(home: MyApp()));
+  // runApp(MaterialApp(home: MyApp()));
+
+  runApp(MaterialApp(home: DemoList()));
+}
+
+class DemoList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(children: <Widget>[
+            ListTile(title:Text("商品列表")),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(title: Text("$index"));
+                },
+                itemCount: 10,
+              ),
+            ),
+          ])
+    ); 
+  }
 }
